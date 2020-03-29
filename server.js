2bc9eec7137137f312f2e5fb5101df036d0b40d1
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-29 23:05:00 +0530

    connect to database

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/database')
 
 dotenv.config({ path: './config/.env' })
+
+connectDB()
 
 app.use('/api/bootcamps', bootcamps)
 
