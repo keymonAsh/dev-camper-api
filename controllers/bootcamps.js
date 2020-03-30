893d47966235fc91f4489cb68d5ef80e4bba19f6
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-30 07:42:31 +0530

    setup SHOW and SHOWBYID routes

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,7 +1,16 @@
 const Bootcamp = require('../models/bootcamp')
 
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({ succsess: true })
+exports.getBootcamps = async (req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find()
+        res.status(200).json({
+            success: true,
+            count: bootcamps.length,
+            data: bootcamps
+        })
+    } catch (err) {
+        res.status(400).json({success: false})
+    }
 }
 
 exports.createBootcamp = async (req, res, next) => {
@@ -16,14 +25,27 @@ exports.createBootcamp = async (req, res, next) => {
     }
 }
 
-exports.showBootcamp = (req, res, next) => {
-    res.status(201).json({ succsess: true })
+exports.showBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id)
+
+        if(!bootcamp) {
+            return  res.status(400).json({success: false})
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({success: false})
+    }
 }
 
 exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({ succsess: true })
+    res.status(200).json({ success: true })
 }
 
 exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({ succsess: true })
+    res.status(200).json({ success: true })
 }
