ac776463effae9cbd363c5a081e82181714ff6b7
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-30 19:28:25 +0530

    Select fields and Sorting functionality

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,9 +4,29 @@ const Bootcamp = require('../models/bootcamp')
 
 // SHOW ALL
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    let queryStr = JSON.stringify(req.query)
+    const reqQuery = { ...req.query } // copy of req.query
+    const removeFields = ['select', 'sort']
+    removeFields.forEach(param => delete reqQuery[param])
+
+    let queryStr = JSON.stringify(reqQuery) // for creating the operator
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
-    const bootcamps = await Bootcamp.find(JSON.parse(queryStr))
+
+    let query = Bootcamp.find(JSON.parse(queryStr))
+
+    if(req.query.select) { // Selecting fields for display
+        const fields = req.query.select.split(',').join(' ')
+        query = query.select(fields)
+    }
+
+    if(req.query.sort) { // Selecting fields for sort
+        const sortBy = req.query.sort.split(',').join(' ')
+        query = query.sort(sortBy)
+    } else {
+        query = query.sort('-createdAt')
+    }
+
+    const bootcamps = await query
+
     res.status(200).json({
         success: true,
         count: bootcamps.length,
