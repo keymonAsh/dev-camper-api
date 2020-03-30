1b61464b3c183a3f12bb1b3de19902d819f5f455
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-30 16:42:22 +0530

    add slugigy with mongoose middleware

--- models/bootcamp.js
+++ models/bootcamp.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const bootcampSchema = new mongoose.Schema({
     name: {
@@ -98,6 +99,11 @@ const bootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+})
+
+bootcampSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, {lower: true})
+    next()
 })
 
 module.exports = mongoose.model('Bootcamp', bootcampSchema)
