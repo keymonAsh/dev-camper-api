6c78a1a5c2b379d47dd45d952b99d7ec054f1d83
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-30 19:51:35 +0530

    pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,7 +5,7 @@ const Bootcamp = require('../models/bootcamp')
 // SHOW ALL
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
     const reqQuery = { ...req.query } // copy of req.query
-    const removeFields = ['select', 'sort']
+    const removeFields = ['select', 'sort', 'page', 'limit']
     removeFields.forEach(param => delete reqQuery[param])
 
     let queryStr = JSON.stringify(reqQuery) // for creating the operator
@@ -25,11 +25,33 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
         query = query.sort('-createdAt')
     }
 
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 25
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Bootcamp.countDocuments()
+    query.skip(startIndex).limit(limit)
+
     const bootcamps = await query
+
+    const pagination = {}
+    if(endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+    if(startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
 
     res.status(200).json({
         success: true,
         count: bootcamps.length,
+        pagination: pagination,
         data: bootcamps
     })
 })
