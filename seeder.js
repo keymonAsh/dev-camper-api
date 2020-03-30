7360326527f277b0bf0d8bfab700af86d5f39b7f
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-30 17:42:33 +0530

    add database seeder

---
+++ seeder.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const colors = require('colors')
+const dotenv = require('dotenv')
+const Bootcamp = require('./models/bootcamp')
+
+dotenv.config({ path: './config/.env' })
+
+mongoose.connect(process.env.MONGO_URI, {
+    useCreateIndex:true,
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useFindAndModify: false
+})
+
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps)
+        console.log('Data Imported.'.green.inverse)
+        process.exit()
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany()
+        console.log('Data Destroyed.'.red.inverse)
+        process.exit()
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+if(process.argv[2] === '-i') {
+    importData()
+} else if(process.argv[2] === '-d') {
+    deleteData()
+}
