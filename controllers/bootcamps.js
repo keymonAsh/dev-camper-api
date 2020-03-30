9c2f0606e29286e557b33dcfbc64580a432c56cc
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-30 09:28:35 +0530

    more ErrorHandling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -22,7 +22,7 @@ exports.createBootcamp = async (req, res, next) => {
             data: bootcamp
         })
     } catch (err) {
-        res.status(400).json({success: false})
+        next(err)
     }
 }
 
