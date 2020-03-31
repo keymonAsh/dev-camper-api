4b21d1541296ad1fa66fb6445b9a264e0d5eba00
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-31 16:47:27 +0530

    Calculate average cost : changes in courses model

--- models/coures.js
+++ models/coures.js
@@ -39,4 +39,34 @@ const courseSchema = new mongoose.Schema({
     }
 })
 
+courseSchema.statics.getAvergeCost = async function(bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: {bootcamp: bootcampId}
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' }
+            }
+        }
+    ]) 
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+courseSchema.post('save', function() {
+    this.constructor.getAvergeCost(this.bootcamp)
+})
+
+courseSchema.pre('save', function() {
+    this.constructor.getAvergeCost(this.bootcamp)
+})
+
 module.exports = mongoose.model('Course', courseSchema)
