1d7e01be77fff7130cd14a0cca09f2dc8d03ee16
Aswin Gangadharan <keymonash9@gmail.com>
2020-04-01 12:02:21 +0530

    user create function and password encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,13 @@ const asyncHandler = require('../middleware/asyncHandeler')
 const User = require('../models/user')
 
 exports.register = asyncHandler(async (req, res, next) => {
+    const {name, email, password, role} = req.body
+    const user = await User.create({
+        name,
+        password,
+        email,
+        role
+    })
     res.status(200).json({
         success: true
     })
